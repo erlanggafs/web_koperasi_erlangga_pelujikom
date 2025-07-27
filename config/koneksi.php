950be034e0c7ad2374d1decbabd045c386ffabf2
erlanggafs <erlanggafs05@outlook.com>
2025-07-27 18:18:24 +0700

    edit port

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
 // mysqli_connect("hostname","user database","password","nama")
-$koneksi= mysqli_connect("localhost:3310","root","","erlang_koperasi_rs") or die("Tidak bisa terhubungan ke database");
+$koneksi= mysqli_connect("localhost:3306","root","","erlang_koperasi_rs") or die("Tidak bisa terhubungan ke database");
 
 
